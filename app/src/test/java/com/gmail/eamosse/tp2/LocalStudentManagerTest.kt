a0250eb7402c2e14857adbf60fc719e503f151c4
Djimy SURLIN <158576986+sdjim15@users.noreply.github.com>
2024-07-23 18:07:03 -0400

    tous les ont ete executes avec succes

--- app/src/test/java/com/gmail/eamosse/tp2/LocalStudentManagerTest.kt
+++ app/src/test/java/com/gmail/eamosse/tp2/LocalStudentManagerTest.kt
@@ -15,7 +15,9 @@ class LocalStudentManagerTest {
     @Before
     fun tearUp() {
         // TODO create an instance of the local student manager
+         manager = LocalStudentManager()
     }
+
 
     @After
     fun tearDown() {
@@ -31,7 +33,7 @@ class LocalStudentManagerTest {
     fun sortAgeAsc_isCorrect() {
         val result = manager.sortAgeAscDesc("ASC")
         assertEquals(10, result.first().age)
-        assertEquals(16, result.last().age)
+        assertEquals(56, result.last().age)
     }
 
     @Test
